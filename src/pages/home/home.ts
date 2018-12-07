cbbdbac0dc9da61b7d7f90b950f0d40c8f61624c
suga0828 <alexander.sf0828@gmail.com>
2018-12-07 18:17:58 -0500

    Slide delete Lugar with toast

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ToastController } from 'ionic-angular';
 
 import { LugarPage } from '../lugar/lugar';
 import { LugaresService } from '../../services/lugares.service';
@@ -14,7 +14,9 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    public lugaresService: LugaresService
+    public lugaresService: LugaresService,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController
   ) {}
 
   ionViewDidLoad() {
@@ -32,6 +34,40 @@ export class HomePage {
 
   toCreate() {
     this.navCtrl.push(LugarPage, { lugar: {} });
+  }
+
+  deleteLugar(lugar) {
+    const confirm = this.alertCtrl.create({
+      title: `¿Desea eliminar ${lugar.nombre}?`,
+      buttons: [
+        {
+          text: 'Eliminar',
+          handler: () => {
+            console.log('Eliminar clicked');
+            let eliminated = lugar.nombre;
+            this.lugaresService.deleteLugar(lugar)
+              .then( () => {
+                this.deleteToast(eliminated);
+              })
+              .catch( error => console.log(error));
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => {}
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  deleteToast(name) {
+    const toast = this.toastCtrl.create({
+      message: `${name} was deleted successfully`,
+      duration: 1500,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
