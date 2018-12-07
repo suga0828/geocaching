8b062672b4554e822d7547167f0d97c3c409b15d
suga0828 <alexander.sf0828@gmail.com>
2018-12-07 14:37:52 -0500

    CRUD in Firebase for Lugares

---
+++ src/services/lugares.service.ts
@@ -0,0 +1,28 @@
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+
+export class LugaresService {
+  constructor(public afDB: AngularFireDatabase) {}
+
+  public getLugares() {
+    return  this.afDB.list('/lugares');
+  }
+
+  public getLugar(id) {
+    return this.afDB.object('/lugares/'+ id);
+  }
+
+  public createLugar(lugar) {
+    return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+  }
+
+  public editLugar(lugar) {
+    return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+  }
+
+  public deleteLugar(lugar) {
+    return this.afDB.database.ref('/lugares/' + lugar.id).remove();
+  }
+}
